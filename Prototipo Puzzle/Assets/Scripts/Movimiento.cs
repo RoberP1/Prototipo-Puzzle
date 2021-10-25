6ad15e703e8990bd22508398702c4dce0afc5b26
RoberP1 <roberpapeschi@gmail.com>
2021-10-25 16:19:47 -0300

    Agregado Script Movimiento

---
+++ Prototipo Puzzle/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    Rigidbody rb = new Rigidbody(); //jugador
+
+    
+
+    public float vel = 1; //velocidad movimiento
+
+    public float sensivilidad = 1; //sensivilidad del mouse
+
+    public Camera FPSCamera; //cabeza
+    
+    //imputs
+    private float horizontalInput;
+    private float verticalInput;
+    private float x;
+    private float y;
+    private float xRotacion;
+
+    
+
+    
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+
+        
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //obtener imput
+
+        //imput movimiento personaje
+        horizontalInput = Input.GetAxis("Horizontal")  ;
+        verticalInput = Input.GetAxis("Vertical")  ;
+
+
+        //movimiento personaje
+        rb.AddRelativeForce(new Vector3(horizontalInput, 0, verticalInput).normalized * vel * Time.deltaTime * 500f);
+
+
+        //imputs movimiento camara
+        x = Input.GetAxis("Mouse X") * sensivilidad;
+        y = Input.GetAxis("Mouse Y") * sensivilidad;
+        xRotacion -= y;
+        
+        xRotacion = Mathf.Clamp(xRotacion, -90f, 41f);  //limitador de movimiento de camara
+
+        
+        //movimiento camara
+        FPSCamera.transform.localRotation = Quaternion.Euler(xRotacion, 0, 0);
+        rb.transform.Rotate(0, x, 0);
+
+        
+
+
+        
+
+    }
+
+}
